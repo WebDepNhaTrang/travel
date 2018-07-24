190b50ffa28c95c5f9d40cbb7dea579ebf86f8f6
Thien Pham <chithien175@gmail.com>
2018-07-24 16:40:09 +0700

    create hotels table

--- routes/route-default.php
+++ routes/route-default.php
@@ -2,25 +2,21 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Routes Default
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Đây là route default để khởi tạo 1 dự án mới
+| 
+| 
 |
 */
 /*
 |--------------------------------------------------------------------------
-| Backend
+| Frontend
 |--------------------------------------------------------------------------
 */
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
 
+// Trang Chủ
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
+    return view('theme-default.pages.home');
+})->name('frontend.pages.home');
