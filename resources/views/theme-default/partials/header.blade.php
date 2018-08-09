c18d4c3157cbbc7a3c788d2edf437e4afccdee86
Thien Pham <chithien175@gmail.com>
2018-08-09 10:33:10 +0700

    update logo

--- resources/views/theme-default/partials/header.blade.php
+++ resources/views/theme-default/partials/header.blade.php
@@ -1,5 +1,13 @@
 
-<h1 id="colorlib-logo"><a href="index.html">Travel</a></h1>
+<h1 id="colorlib-logo">
+    @if(setting('site.logo'))
+    <a href="{{ route('frontend.pages.home') }}">
+        <img src="{{ Voyager::image(setting('site.logo')) }}" alt="Aloroom Logo" width="170">
+    </a>
+    @else
+    <a href="{{ route('frontend.pages.home') }}">{{ setting('site.title') }}</a>
+    @endif
+</h1>
 <nav id="colorlib-main-menu" role="navigation">
     {{ menu('frontend', 'theme-default.partials.main-menu') }}
 </nav>
